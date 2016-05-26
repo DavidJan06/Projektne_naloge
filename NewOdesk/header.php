357be685d4e6770f985e81db49b0931dc932880a
dijak <dijak@dijak.com>
2016-05-26 11:18:40 +0200

    David

--- NewOdesk/header.php
+++ NewOdesk/header.php
@@ -11,24 +11,21 @@ session_start();
             <div id="">
                 <div id="">
                     <ul>
+                        <?php if (!isset($_SESSION['user_id'])) {
+                             ?>
+                            <li><a href="user_add.php"><span></span>Registracija</a></li>
+                            <li><a href="login.php"><span></span>Prijava</a></li>
+                        <?php
+                        } 
+                        else{?>
                         <li><a href="index.php"><span></span>Domov</a></li>
                         <li><a href="countries.php"><span></span>Države</a></li>
                         <li><a href="skills.php"><span></span>Veščine</a></li>
-                        <?php
-                        if (isset($_SESSION['user_id'])) {
-                            ?>
-                            <li><a href="logout.php"><span></span>Odjava</a></li>
-                            <li><a href="profile.php"><span></span>
-                                    <?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>
-                                </a></li>
-                            <?php
-                        } else {
-                            ?>                
-                            <li><a href="user_add.php"><span></span>Registracija</a></li>
-                            <li><a href="login.php"><span></span>Prijava</a></li>
-                            <?php
-                        }
-                        ?>
+                        <li><a href="logout.php"><span></span>Odjava</a></li>
+                        <li><a href="profile.php"><span></span>
+                            <?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>
+                            </a></li>
+                        <?php } ?>
                     </ul>    	
                 </div>
             </div>
