8982655f084c829da74b5054fff2149f04742ae1
dijak <dijak@dijak.com>
2016-06-09 10:55:40 +0200

    David

--- NewOdesk/footer.php
+++ NewOdesk/footer.php
@@ -1,9 +1,9 @@
-</div>
 <div id="foot">
     <div id="a">
        	Copyright © 2016 <a href="#">3.TRA</a> - Designed by <a href="#">ERŠ</a>
         <div class=""></div>
     </div>
-</div>       
+</div>   
+</div>    
 </body>
 </html>
